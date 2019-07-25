27605a274580929bafd1b18c5e861ef950383cd7
YosiLeibman <yosil.770@gmail.com>
2019-07-25 15:10:36 +0300

    test commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', function(socket) {
+io.on('connection', socket => {
 	console.log('a user connected')
 	socket.on('disconnect', function() {
 		console.log('user disconnected')
