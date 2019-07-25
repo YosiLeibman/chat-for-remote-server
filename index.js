d5173ed65b3ffc2e1fe3b92abf9a507921d9e86f
YosiLeibman <yosil.770@gmail.com>
2019-07-25 08:37:25 +0300

    works for remote server tests

---
+++ index.js
@@ -0,0 +1,25 @@
+const app = require('express')()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket) {
+	console.log('a user connected')
+	socket.on('disconnect', function() {
+		console.log('user disconnected')
+	})
+	socket.on('snd-msg', msg => {
+		console.log('message: ' + msg)
+		io.emit('brodcast-msg', msg)
+	})
+	socket.on('someone-typing', msg => {
+		console.log(msg)
+	})
+})
+
+server.listen(3033, _ => {
+	console.log('server started on port 3033')
+})
